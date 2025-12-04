8f789e159be90f038dd02b7ad00cd58e057ccd4e
Braian Torres <brianntorress.s@gmail.com>
2025-12-04 02:24:07 -0300

    refactor(project): get projectId parameter from URL, use to fetch project data and set info using domservice methods

--- scripts/project.js
+++ scripts/project.js
@@ -1,15 +1,22 @@
 import { setText, fillList, renderTechnologies } from "../services/domservice.js";
 
+const params = new URLSearchParams(window.location.search);
+const projectId = params.get("id")
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("mock/project.json")
+    fetch(`http://127.0.0.1:8000/project/${projectId}`)
     .then(response => response.json())
     .then(data =>{
 
-        setText("about-text", data.about);
-        fillList("features-list", data.features);
+        const featStrings = data.feats.map(f => f.feat);
+        const infoStrings = data.technical_info.map(i => i.info);
+
+        setText("project-name", data.name);
+        setText("about-text", data.big_about);
+        fillList("features-list", featStrings);
         renderTechnologies(data.techs);
-        fillList("tech-info-list", data.techInfo);
-        setText("user-comment", data.userComment);
+        fillList("tech-info-list", infoStrings);
+        setText("user-comment", data.user_comment);
 
     })
     .catch(error => console.log("Error al obtener los datos: ", error));
