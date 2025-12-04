6bdb6522815c6dedc4f9523145be252f57705685
Braian Torres <brianntorress.s@gmail.com>
2025-12-04 02:31:39 -0300

    refactor(render): update DOM mapping to new API response schema and image URLs

--- services/indexdomservice.js
+++ services/indexdomservice.js
@@ -1,9 +1,9 @@
 export function renderUserInfo(portfolio){
 
-    document.getElementById("user-name").textContent = portfolio.userName;
-    document.getElementById("user-profession").textContent = portfolio.userProfession;
-    document.getElementById("user-photo").textContent = portfolio.userPhoto;
-    document.getElementById("user-about").textContent = portfolio.userAbout;
+    document.getElementById("user-name").textContent = portfolio.user_name;
+    document.getElementById("user-profession").textContent = portfolio.user_profession;
+    document.getElementById("user-photo").src = "http://127.0.0.1:8000" + portfolio.user_photo;
+    document.getElementById("user-about").textContent = portfolio.user_about;
 
 }
 
@@ -14,9 +14,10 @@ export function renderProjects(userProjects){
 
     userProjects.forEach(project => {
         const clone = projectTemplate.content.cloneNode(true);
-        clone.querySelector(".project-img").src = project.img;
+        clone.querySelector(".project-item").dataset.projectId = project.id
+        clone.querySelector(".project-img").src = "http://127.0.0.1:8000" + project.cover_src;
         clone.querySelector(".project-name").textContent = project.name;
-        clone.querySelector(".project-small-about").textContent = project.smallAbout;
+        clone.querySelector(".project-small-about").textContent = project.small_about;
 
         projectContainer.appendChild(clone);
     });
